b66e691963e3dd62f3fb57e6c6c865c9a7684141
darrk-heart <prospaul999@gmail.com>
2024-02-03 16:54:42 +0000

    list rendering

---
+++ src/components/NameList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Person from "./Person";
+
+function NameList() {
+  const persons = [
+    {
+      id: 1,
+      name: "Theo",
+      age: 30,
+      skill: "hack",
+    },
+    {
+      id: 2,
+      name: "Mat",
+      age: 19,
+      skill: "sing",
+    },
+    {
+      id: 3,
+      name: "Bervlyn",
+      age: 23,
+      skill: "code",
+    },
+    {
+      id: 4,
+      name: "Scott",
+      age: 28,
+      skill: "dance",
+    },
+  ];
+  const personList = persons.map((person) => <Person person={person} />);
+  return <div>{personList}</div>;
+}
+
+export default NameList;
