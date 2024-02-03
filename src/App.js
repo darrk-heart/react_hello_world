8da8cc7f2dd71c82dcdc4736501c8026ce009a28
darrk-heart <prospaul999@gmail.com>
2024-02-03 15:39:22 +0000

    writing methods as props

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
 
 function App() {
   return (
     <div className="App">
+      <ParentComponent/>
       {/* <FunctionClick/> */}
       {/* <ClassClick /> */}
-      <EventBind/>
+      {/* <EventBind/> */}
       {
         // <Greet name="Ofosu" heroName="SuperBoy" />
         // <Counter />
