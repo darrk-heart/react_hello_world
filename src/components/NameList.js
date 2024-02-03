6bb309a1d8633b11079b8cbc173a94a8b4b84dfb
darrk-heart <prospaul999@gmail.com>
2024-02-03 22:52:46 +0000

    list and keys

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -28,7 +28,7 @@ function NameList() {
       skill: "dance",
     },
   ];
-  const personList = persons.map((person) => <Person person={person} />);
+  const personList = persons.map((person) => <Person key={person.id} person={person} />);
   return <div>{personList}</div>;
 }
 
