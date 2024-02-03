4b4abb0a1091864901ada01dd2ce0b3e8787a133
darrk-heart <prospaul999@gmail.com>
2024-02-03 15:58:31 +0000

    writing methods as props

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react'
-import ChildComponent from './ChildComponent'
+import React, { Component } from "react";
+import ChildComponent from "./ChildComponent";
 
 class ParentComponent extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         parentName: "Kweku"
-      }
+    this.state = {
+      parentName: "Kweku",
+    };
 
-      this.greetParent = this.greetParent.bind(this)
-    }
+    this.greetParent = this.greetParent.bind(this);
+  }
 
-    greetParent(childName) {
-        alert(`Hello ${this.state.parentName} from ${childName}`)
-    }
+  greetParent(childName) {
+    alert(`Hello ${this.state.parentName} from ${childName}`);
+  }
   render() {
     return (
       <div>
-        <ChildComponent greetHandler={this.greetParent}/>
+        <ChildComponent greetHandler={this.greetParent} />
       </div>
-    )
+    );
   }
 }
 
-export default ParentComponent
+export default ParentComponent;
---
+++ src/components/UserGreeting.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+     isLoggedIn: false
+  }
+}
+    
+  render() {
+    return (
+      <div>Welcome Kwame</div>
+    )
+  }
+}
+
+export default UserGreeting
