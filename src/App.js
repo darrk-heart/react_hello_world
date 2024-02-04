f067e2988d295aa1be1af93437c55009eaab1ee3
darrk-heart <prospaul999@gmail.com>
2024-02-04 00:55:10 +0000

    styling and css basics

--- src/App.js
+++ src/App.js
@@ -11,11 +11,19 @@ import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
 import NameList from "./components/NameList";
 import KeyasIndex from "./components/KeyasIndex";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
 
 function App() {
   return (
     <div className="App">
-      <KeyasIndex />
+      <h1 className="error">Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      {/* <Inline/> */}
+      {/* <Stylesheet primary={true}/> */}
+      {/* <KeyasIndex /> */}
       {/* <NameList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent/> */}
